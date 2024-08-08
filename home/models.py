2652bf31dd6dd648aa19e05d501c7bbe98317f28
Anthony <aurbina5@rockets.utoledo.edu>
2024-08-08 19:48:10 -0400

    first commit

---
+++ home/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+# Create your models here.
+# class Upload(models.Model):
+#     upload_file = models.FileField()    
+#     upload_date = models.DateTimeField(auto_now_add =True)
---
+++ odot_v2/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for odot_v2 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from odot_v2 import views as v 
+# from uploader import views as uploader_views
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+    #path("signup/", SignUpView.as_view(), name="signup"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    # path('', uploader_views.UploadView.as_view(), name='fileupload'), 
+    # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+]
